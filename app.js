f1a7c4ea2ce7ef75900ebc1a2a038d3f5c177ad4
baenshee <florian.fiol@ynov.com>
2017-04-06 11:50:24 +0000

    Project routes

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var projects = require('./routes/projects');
 
 var mongoose = require('mongoose');
 var app = express();
@@ -30,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/project', projects);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/projects.js
+++ routes/projects.js
@@ -7,35 +7,35 @@ const mongoose = require ('mongoose');
 const projectModel = require('../model/ProjectModel');
 
 /* GET projects listing. */
-router.get('/project', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.send('respond with a resource');
   projectModel.getAllProjectsName((data)=>{
       res.send(data);
   });
 });
 
-router.get('/project/:id', function(req, res, next) {
+router.get('/:id', function(req, res, next) {
   res.send('respond with a resource');
   projectModel.getProjectById(req.params.id,(data)=>{
       res.send(data);
   });
 });
 
-router.post('/project', function(req, res, next) {
+router.post('/', function(req, res, next) {
   res.send('respond with a resource');
   projectModel.getProjectById(req.body.project,(data)=>{
       res.send(data);
   });
 });
 
-router.put('/project', function(req, res, next) {
+router.put('/', function(req, res, next) {
   res.send('respond with a resource');
   projectModel.getProjectById(req.body.project,(data)=>{
       res.send(data);
   });
 });
 
-router.delete('/project/:id', function(req, res, next) {
+router.delete('/:id', function(req, res, next) {
   res.send('respond with a resource');
   projectModel.deleteProject(req.params.id,(data)=>{
       res.send(data);
