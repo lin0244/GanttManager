22e5dbc509bcf5412360167b3d708d022aa3d6ce
baenshee <florian.fiol@ynov.com>
2017-04-07 16:11:41 +0000

    Socket.io for projects management

--- app.js
+++ app.js
@@ -1,24 +1,22 @@
 'use strict';
-var http = require('http');
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
-var auth = require('./routes/auth');
-
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-var projects = require('./routes/projects');
-
-var mongoose = require('mongoose');
-var app = express();
-
-var server = http.createServer(app);
+const http = require('http');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const index = require('./routes/index');
+const users = require('./routes/users');
+const auth = require('./routes/auth');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const mongoose = require('mongoose');
+const app = express();
+const server = http.createServer(app);
+const io= require('socket.io').listen(server);
+const projects = require('./routes/projects')(app,io);
+const CronJob = require('cron').CronJob;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -44,7 +42,7 @@ app.use('/project', projects);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  let err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
@@ -67,13 +65,13 @@ mongoose.createConnection('mongodb://localhost/ganttmanager', (error) => {
 })
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
-  var addr = server.address();
+  let addr = server.address();
   console.log("GanttManager listening at ", addr.address + ":" + addr.port);
 });
 
 //Cron pour vider les Tokens du Serveur - Tous les soirs à Minuit
-var CronJob = require('cron').CronJob;
-var job = new CronJob('00 00 00 * * *', () => {
+
+let job = new CronJob('00 00 00 * * *', () => {
     auth.deleteTokens();
   },
   null,
--- routes/projects.js
+++ routes/projects.js
@@ -3,12 +3,18 @@
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
-const mongoose = require ('mongoose');
+const mongoose = require('mongoose');
 const projectModel = require('../model/ProjectModel');
 
 
 const socket = require('socket.io-client');
-let client = socket.connect('https://c9.seefox.fr', {reconnect: true});
+let sockets=[];
+let client = socket.connect('https://c9.seefox.fr', {
+  reconnect: true
+});
+
+const serviceName = "qwertyuiop";
+
 
 client.on('connect', () => {
   console.log('connected')
@@ -16,72 +22,90 @@ client.on('connect', () => {
   client.emit('needHelp');
 });
 
-client.on('info',(data)=>{
+client.on('info', (data) => {
   console.log(data);
 })
- 
-const serviceName="qwertyuiop";
 
-/* GET projects listing. */
-router.get('/', function(req, res, next) {
-    res.render('project/index', { title: 'Express' });
-});
-
-router.get('/all', function(req, res, next) {
-  projectModel.getAllProjectsName((data)=>{
-      res.send(data);
+module.exports = (router, io) => {
+  io.on('connect',(socket)=>{
+    let temp={};
+    temp.socket=socket;
+    temp.projectName=socket.handshake.query['projectName'];
+    sockets.push(temp);
   });
-});
-
-router.get('/:id', function(req, res, next) {
-  projectModel.getProjectById(req.params.id,(data)=>{
-      res.send(data);
+  
+  io.on('disconnect',(socket)=>{
+    sockets.splice(sockets.indexOf(socket), 1);
   });
-});
+  
+  
+  /* GET projects listing. */
+  router.get('/', function(req, res, next) {
+    res.render('project/index', {
+      title: 'Express'
+    });
+  });
 
-router.post('/', function(req, res, next) {
-  let toSend={};
-  toSend.serviceName=serviceName;
-  toSend.projects=[];
-  toSend.projects.push(req.body.project);
-  req.body.project.serviceName = serviceName;
-  projectModel.createProject(req.body.project,(data)=>{
+  router.get('/all', function(req, res, next) {
+    projectModel.getAllProjectsName((data) => {
+      res.send(data);
+    });
+  });
+
+  router.get('/:id', function(req, res, next) {
+    projectModel.getProjectById(req.params.id, (data) => {
+      res.send(data);
+    });
+  });
+
+  router.post('/', function(req, res, next) {
+    let toSend = {};
+    toSend.serviceName = serviceName;
+    toSend.projects = [];
+    toSend.projects.push(req.body.project);
+    req.body.project.serviceName = serviceName;
+    projectModel.createProject(req.body.project, (data) => {
       updateService(toSend);
       res.send(data);
+    });
   });
-});
 
-router.put('/', function(req, res, next) {
-  projectModel.updateProject(req.body.project,(data)=>{
-    updateService(req.body.project);
+  router.put('/', function(req, res, next) {
+    projectModel.updateProject(req.body.project, (data) => {
+      updateService(req.body.project);
+      sockets.forEach((s)=>{
+        if(s.projectName==req.body.project.name){
+          s.emit('updateProject', req.body.project);
+        }
+      });
       res.send(data);
+    });
   });
-});
 
-router.delete('/:id', function(req, res, next) {
-  projectModel.deleteProject(req.params.id,(data)=>{
+  router.delete('/:id', function(req, res, next) {
+    projectModel.deleteProject(req.params.id, (data) => {
       res.send(data);
+    });
   });
-});
+};
 
-function deleteService(){
-  client.emit('deleteService',serviceName);
+function deleteService() {
+  client.emit('deleteService', serviceName);
 }
 
-function updateService(data){
-  client.emit('sendUpdate',data);
+function updateService(data) {
+  client.emit('sendUpdate', data);
 }
 
-client.on('errorOnProjectUpdate',(data)=>{
+client.on('errorOnProjectUpdate', (data) => {
   console.log(data);
 });
 
-client.on('projectUpdated',(data)=>{
-  for(let d in data){
-    projectModel.createExternalProject(d,(err,data)=>{
-      if(err)
+client.on('projectUpdated', (data) => {
+  for (let d in data) {
+    projectModel.createExternalProject(d, (err, data) => {
+      if (err)
         console.log(err)
     });
   }
 });
-module.exports = router;
