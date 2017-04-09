a60f01070300a80113ef265a76fa28314ae4c8c7
Thibaud Peytavy <thibaud.peytavy@ynov.com>
2017-04-09 18:22:14 +0200

    ces fichiers ce sont échappé ?

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,5 +1,5 @@
 /**
  * Created by leino on 06/04/2017.
  */
-var app = angular.module('app', ['ngMaterial', 'ngCookie', 'angularMoment', 'gantt', 'gantt.table', 'gantt.movable', 'gantt.tooltips']);
+var app = angular.module('app', ['ngMaterial', 'ngCookies', 'angularMoment', 'gantt', 'gantt.table', 'gantt.movable', 'gantt.tooltips']);
 app.value('serverData', window.serverData)
